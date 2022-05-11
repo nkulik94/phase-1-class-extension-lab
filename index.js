e28689d3dd4593cab91574b58fe829e4a65c03f6
Naftali Kulik <nkulik1994@gmail.com>
2022-05-11 15:32:42 -0400

    commit tree 04f4b967e311f218ce4592fe77fc7fd06d1321b7

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides
+    }
+
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {
+        return this.sides.reduce((sum, side) => sum + side, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        return this.countSides === 3 && this.sides.find(side => (this.perimeter - side) < side) === undefined
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return this.countSides === 4 && this.sides.find(side => side !== this.perimeter / 4) === undefined
+    }
+
+    get area() {
+        return this.isValid === true ? this.sides[0] ** 2 : 'Sorry dude not a square'
+    }
+}
